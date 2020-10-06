6b441c964bc6534cc8cdc510f988c0da3e7b81c8
Damian Evans <damian@bluesky.eu.com>
2020-10-06 22:37:40 +0100

    Added a test using Mockito

--- src/test/java/org/damo/tests/CharacterCopierShould.java
+++ src/test/java/org/damo/tests/CharacterCopierShould.java
@@ -1,9 +1,14 @@
 package org.damo.tests;
 
 import org.damo.CharacterCopier;
+import org.damo.interfaces.Destination;
+import org.damo.interfaces.Source;
 import org.damo.tests.testdoubles.DestinationSpy;
 import org.damo.tests.testdoubles.SourceStub;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class CharacterCopierShould {
@@ -44,5 +49,28 @@ public class CharacterCopierShould {
             CharacterCopier copier=new CharacterCopier(sourceStub, destinationSpy);
             copier.copy();
         } ).hasMessage("Source has no newline character");
+    }
+
+    @Test
+    public void CopyABC_WhenGiven_ABCandNewline_UsingMock() throws Exception {
+        //arrange
+        Source mockSource = mock(Source.class);
+        Destination mockDestination = mock(Destination.class);
+        when(mockSource.validate())
+                .thenReturn(true);
+        when(mockSource.getChar())
+                .thenReturn('A','B','C', '\n');
+
+        //act
+        CharacterCopier copier=new CharacterCopier(mockSource, mockDestination);
+        copier.copy();
+
+        //assert
+        verify(mockDestination, times(1) ).setChar('A');
+        verify(mockDestination, times(1) ).setChar('B');
+        verify(mockDestination, times(1) ).setChar('C');
+        verify(mockDestination, never() ).setChar('\n');
+
+
     }
 }
